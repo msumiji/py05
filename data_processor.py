339ce786b1dddd00753f627ed4b424e84da3f3c5
Masunobu Sumiji <sumijim@gmail.com>
2026-09-08 19:23:20 +0900

    aho

--- data_processor.py
+++ data_processor.py
@@ -33,7 +33,7 @@ class NumericProcessor(DataProcessor):
         if self.validate(data) == False:
             print("Got exception: Improper numeric data")
             return
-        start_n = len(self.values) + 1
+        start_n = len(self.values)
         if isinstance(data, list):
             for n, dn in enumerate(data, start=start_n):
                 self.values.append((n, str(dn)))
@@ -44,7 +44,7 @@ class NumericProcessor(DataProcessor):
 class TextProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
         if isinstance(data, str):
-                    return True
+            return True
         if isinstance(data, list):
             return all(
             isinstance(item, str)
@@ -57,31 +57,82 @@ class TextProcessor(DataProcessor):
         if self.validate(data) == False:
             print("Got exception: Improper text data")
             return
-        start_n = len(self.values) + 1
+        start_n = len(self.values)
         if isinstance(data, list):
             for n, text in enumerate(data, start=start_n):
                 self.values.append((n, text))
         else:
             self.values.append((start_n,data))
 
+class LogProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, dict) and set(data.keys()) == {"log_level", "log_message"}:
+            return True
+        if isinstance(data, list):
+            return all(
+            isinstance(item, dict) and set(item.keys()) == {"log_level", "log_message"}
+            for item in data
+            )
+        else:
+            return False
+
+    def ingest(self, data: Any) -> None:
+        if self.validate(data) == False:
+            print("Got exception: Improper log data")
+            return
+        start_n = len(self.values)
+        if isinstance(data, list):
+            for n, log in enumerate(data, start=start_n):
+                value1 = log["log_level"]
+                value2 = log["log_message"]
+                text = f"{value1}: {value2}"
+                self.values.append((n, text))
+        else:
+            value1 = data["log_level"]
+            value2 = data["log_message"]
+            text = f"{value1}: {value2}"
+            self.values.append((start_n,text))
+
 def main() -> None:
-    print("=== Code Nexus - Data Processor ===")
+    print("=== Code Nexus - Data Processor ===\n")
+    print("Testing Numeric Processor...")
     processor1 = NumericProcessor()
-    result1 = processor1.validate("hello")
-    result2 = processor1.validate([876,5678])
-    print(result1, result2)
-    processor1.ingest([876,5678,7])
+    result1 = processor1.validate(42)
+    print(f"Trying to validate input '42': {result1}")
+    result2 = processor1.validate("Hello")
+    print(f"Trying to validate input 'Hello': {result2}")
+    print("Test invalid ingestion of string 'foo' without prior validation")
+    processor1.ingest("foo")
+    data1 = [1, 2, 3, 4, 5]
+    print(f"Processing data: {data1}")
+    processor1.ingest(data1)
+    print("Extracting 3 values...")
     for _ in range(3):
-        numeric_values = processor1.output()
-        print(f"Numeric value:{numeric_values[0]}:{numeric_values[1]}")
+        result3 = processor1.output()
+        print(f"Numeric value {result3[0]}: {result3[1]}")
+    print()
+    print("Testing Text Processor...")
     processor2 = TextProcessor()
-    result3 = processor2.validate(["abc",321])
-    print(result3)
-    processor2.ingest(["Hello","good morning"])
+    result4 = processor2.validate(42)
+    print(f"Trying to validate input '42': {result4}")
+    data2 = ['Hello', 'Nexus', 'World']
+    print(f"Processing data: {data2}")
+    processor2.ingest(data2)
+    print("Extracting 1 value")
     text_values = processor2.output()
-    print(f"Text value:{text_values[0]}:{text_values[1]}")
-    text_values = processor2.output()
-    print(f"Text value:{text_values[0]}:{text_values[1]}")
+    print(f"Text value {text_values[0]}: {text_values[1]}\n")
+    print("Testing Log Processor...")
+    processor3 = LogProcessor()
+    data3 = 'Hello'
+    result5 = processor3.validate(data3)
+    print(f"Trying to validate input '{data3}': {result5}")
+    data4 = [{'log_level': 'NOTICE', 'log_message': 'Connection to server'}, {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}]
+    print(f"Processing data: {data4}")  
+    processor3.ingest(data4)
+    print("Extracting 2 values")
+    for _ in range (2):
+        log_values = processor3.output()
+        print(f"Log entry {log_values[0]}:{log_values[1]}")
 
 
 if __name__ == "__main__":
